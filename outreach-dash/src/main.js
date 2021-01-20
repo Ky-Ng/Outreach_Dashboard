fb3178cecc0c397d85382d3c97e607a98dab1d7c
kyleng <ng.kyle101@gmail.com>
2021-01-19 16:50:35 -0800

    Fix Authentication Firebase lag

--- outreach-dash/src/main.js
+++ outreach-dash/src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import { projectAuth } from './firebase/config'
+
+
+let app
+//only create the app and mount to the DOM when the user first logs in
+projectAuth.onAuthStateChanged(() => { 
+    if(!app){
+        app = createApp(App).use(router).mount('#app')
+    }
+})
+
