33d06191ee41ff4e13e0171e2449d6964b7fc88e
kyleng <ng.kyle101@gmail.com>
2021-01-28 11:12:07 -0800

    temporary not working with vue router mount DOM

---
+++ dash-vue2/src/main.js
@@ -0,0 +1,17 @@
+import router from './router'
+import './assets/main.css'
+
+import { projectAuth } from './firebase/config'
+
+
+let app
+//only create the app and mount to the DOM when the user first logs in
+projectAuth.onAuthStateChanged(() => { 
+    if(!app){
+        // app = createApp(App).use(router).mount('#app')
+        app = new Vue({
+            router
+          }).$mount('#app')
+    }
+})
+
---
+++ dash-vue2/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+import { projectAuth } from '../firebase/config'
+
+// auth guard
+
+//to is where we're going, from is where we're at, next is a func action 
+const requireAuthRouteGuard = (to, from, next) =>{
+  let user = projectAuth.currentUser //this will automatically update
+  if (!user){
+    next({ name: 'Welcome' })
+  }else{
+    next() //carrying on to the "next" route
+  }
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome,
+  },
+  // {
+  //   path: '/home',
+  //   name: 'Home',
+  //   component: Home,
+  //   beforeEnter: requireAuthRouteGuard //just calls the requireAuthRouteGuard
+  // },
+  // {
+  //   path: '/test',
+  //   name: 'ProjectDetail',
+  //   component: ProjectDetail,
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
