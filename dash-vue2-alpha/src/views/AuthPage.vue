08b0fa6bde04716e48a4ca4a34ca24dd6d953011
kyleng <ng.kyle101@gmail.com>
2021-02-02 00:03:30 -0800

    working backend google authentication!

--- dash-vue2-alpha/src/views/AuthPage.vue
+++ dash-vue2-alpha/src/views/AuthPage.vue
@@ -1,24 +1,24 @@
-<!--<template>-->
-<!--  <div>-->
-<!--    <p>Authenticaste pease</p>-->
-<!--    <div id="firebaseui-auth=container"></div>-->
-<!--  </div>-->
-<!--</template>-->
+<template>
+  <div>
+    <p>Please Authenticate</p>
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
 
-<!--<script>-->
-<!--import * as firebaseui from "firebaseui"-->
-<!--import {myProjectAuth} from "@/backend/backend";-->
-<!--export default {-->
-<!--  name: "auth",-->
-<!--  mounted(){ //when it is mounted-->
-<!--    var uiConfig = {-->
-<!--      signInSuccessUrl: '/Home',-->
-<!--      signInOptions:[-->
-<!--          myProjectAuth.GoogleAuthProvider.PROVIDER_ID,-->
-<!--      ]-->
-<!--    };-->
-<!--    var ui = new firebaseui.auth.AuthUI(myProjectAuth);-->
-<!--    ui.start('firebaseui-auth-container', uiConfig)-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
+<script>
+import * as firebaseui from "firebaseui"
+import firebase from "firebase";
+export default {
+  name: "auth",
+  mounted(){ //when it is mounted
+    var uiConfig = {
+      signInSuccessUrl: '/home',
+      signInOptions:[
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ]
+    };
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }
+}
+</script>
