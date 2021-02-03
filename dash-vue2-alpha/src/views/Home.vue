218bd74a904e5cfed75c04a5e82db21d2d36dac1
kyleng <ng.kyle101@gmail.com>
2021-02-02 23:38:10 -0800

    Initialized Welcome Card with dynamic user

--- dash-vue2-alpha/src/views/Home.vue
+++ dash-vue2-alpha/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <p>Home page</p>
+  <v-container>
+  <v-card
+      class="mx-auto"
+      color="#26c6da"
+      dark
+      max-width="3000"
+  >
+    <v-card-title>
+      <h3>Welcome back {{ USER }}. Here is your Serendipity Outreach Dashboard!</h3>
+    </v-card-title>
+  </v-card>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 
 
+import {myProjectAuth} from "@/backend/backend";
+
 export default {
   name: 'Home',
   components: {
-
+  },
+  data (){
+    return{
+      USER: myProjectAuth.currentUser.displayName
+    }
   }
 }
 </script>
