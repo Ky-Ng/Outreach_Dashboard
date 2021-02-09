76a9bd748d6f6e0145e720c4e538c36d208429c9
kyleng <ng.kyle101@gmail.com>
2021-02-08 18:25:34 -0800

    somewhat working

--- dash-vue2-alpha/src/views/Home.vue
+++ dash-vue2-alpha/src/views/Home.vue
@@ -2,6 +2,69 @@
   <v-container>
     <template>
       <v-card class="overflow-hidden">
+        <v-app-bar
+            absolute
+            color="#fcb69f"
+            dark
+            shrink-on-scroll
+
+            src=""
+            scroll-target="#scrolling-techniques-2"
+        >
+          <template v-slot:img="{ props }">
+            <v-img
+                v-bind="props"
+                gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+                min-height="350"
+            ></v-img>
+          </template>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-app-bar-title>Title</v-app-bar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <v-sheet
+            id="scrolling-techniques-2"
+            class="overflow-y-auto"
+            max-height="600"
+        >
+          <v-container style="height: 1000px;"></v-container>
+        </v-sheet>
+      </v-card>
+    </template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <template>
+      <v-card class="overflow-hidden">
 
         <v-app-bar
             absolute
@@ -47,7 +110,7 @@
           max-height="350"
       ></v-img>
       <v-card-title>
-        <h3>Welcome Back {{ USER }}</h3>
+        <h3>Welcome Back {{ USER.displayName }}</h3>
       </v-card-title>
     </v-card>
 
@@ -69,7 +132,7 @@ export default {
   },
   data() {
     return {
-      USER: myProjectAuth.currentUser.displayName,
+      USER: myProjectAuth.currentUser,
 
     }
   }
