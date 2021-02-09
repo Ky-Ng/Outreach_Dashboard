816796d4e4f6156edf14ab87039600062f407e25
kyleng <ng.kyle101@gmail.com>
2021-02-08 20:45:44 -0800

    Added switch

--- dash-vue2-alpha/src/views/Home.vue
+++ dash-vue2-alpha/src/views/Home.vue
@@ -1,120 +1,36 @@
 <template>
   <v-container>
-    <template>
-      <v-card class="overflow-hidden">
-        <v-app-bar
-            absolute
-            color="#fcb69f"
-            dark
-            shrink-on-scroll
-
-            src=""
-            scroll-target="#scrolling-techniques-2"
-        >
-          <template v-slot:img="{ props }">
-            <v-img
-                v-bind="props"
-                gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-                min-height="350"
-            ></v-img>
-          </template>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-          <v-app-bar-title>Title</v-app-bar-title>
-
-          <v-spacer></v-spacer>
-
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
-
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </v-app-bar>
-        <v-sheet
-            id="scrolling-techniques-2"
-            class="overflow-y-auto"
-            max-height="600"
-        >
-          <v-container style="height: 1000px;"></v-container>
-        </v-sheet>
-      </v-card>
-    </template>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <template>
-      <v-card class="overflow-hidden">
-
-        <v-app-bar
-            absolute
-            color="#6A76AB"
-            dark
-            fade-img-on-scroll
-            height="400px"
-            prominent
-            scroll-target="#scrolling-techniques-3"
-            shrink-on-scroll
-            src="@/assets/FullLOGO.png"
-        >
-
-<!--          <template v-slot:img="{ props }">-->
-<!--            <v-img-->
-<!--                v-bind="props"-->
-<!--                gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"-->
-<!--            ></v-img>-->
-<!--          </template>-->
-
-          <!--          the top right section -->
-          <v-container
-              style="min-height: 400px"
-          >
-
-          </v-container>
-        </v-app-bar>
-
-      </v-card>
-
-
-    </template>
-
     <v-card
-        src="@/assets/FullLOGO.png"
         class="mx-auto"
         color="red accent-2"
         dark
         max-width="3000"
+        src="@/assets/FullLOGO.png"
     >
       <v-img
-          src="@/assets/FullLOGO.png"
-          max-height="350"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          max-height="200"
+          src="@/assets/CropLOGO.png"
       ></v-img>
       <v-card-title>
         <h3>Welcome Back {{ USER.displayName }}</h3>
       </v-card-title>
-    </v-card>
+    </v-card> <!-- Header -->
 
-    <CurriculumMain></CurriculumMain>
+    <v-row>
+      <p style="padding-right: 20px; margin-top: 20px"
+      >Change View</p>
+      <v-switch
+          v-model="show_curriculum"
+          color="teal accent-2"
+          inset
+      ></v-switch>
+    </v-row> <!--    Switch   -->
+
+    <CurriculumMain v-if="show_curriculum"></CurriculumMain>
+
+
+
   </v-container>
 </template>
 
@@ -130,10 +46,11 @@ export default {
   components: {
     CurriculumMain,
   },
+
   data() {
     return {
       USER: myProjectAuth.currentUser,
-
+      show_curriculum: true,
     }
   }
 }
