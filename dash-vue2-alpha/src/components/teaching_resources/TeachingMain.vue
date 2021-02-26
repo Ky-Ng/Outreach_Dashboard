fe363e7ba001e2fb1e4cccc0815937e9ee286ff3
kyleng <ng.kyle101@gmail.com>
2021-02-25 21:41:17 -0800

    Quickly added TeachingMain.vue

--- dash-vue2-alpha/src/components/teaching_resources/TeachingMain.vue
+++ dash-vue2-alpha/src/components/teaching_resources/TeachingMain.vue
@@ -11,6 +11,7 @@
         </v-expansion-panel-header>
 
         <v-expansion-panel-content>
+          <v-list-item :href=" `https://bit.ly/serendipitySheet`" target="_blank">Schedule and Sign Ups</v-list-item>
           <v-list-item :href=" `https://docs.google.com/document/d/178hD_GJt7F6s6zoXuBdKOhOvl7tQYQrfk_CzOCP3VHo/edit?usp=sharing`" target="_blank">Curriculum Template
           </v-list-item>
           <v-list-item :href="`https://docs.google.com/document/d/15yU0rux8ty0kOUO1TVZrNz9ihbC47s-Bzt3gSuBWiAg/edit?usp=sharing`" target="_blank">Teaching Process
