bca5a05b859eee1b420284655939acd68c373af7
kyleng <ng.kyle101@gmail.com>
2021-04-22 12:20:43 -0700

    Fixed the Safari support error by removing firebase-admin adding

--- dash-vue2-alpha/functions/index.js
+++ dash-vue2-alpha/functions/index.js
@@ -1,27 +1,30 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin")
-
-admin.initializeApp()
-
-// data is the user information like email or username
-// context authentication information from firebase
-//exports is used to send out a function, https on call just means the method is onCall
-// or availible from the frontend
-//Net Ninja tutorial: https://www.youtube.com/watch?v=4wa3CMK4E2Y
-exports.addAdminRole = functions.https.onCall((data, context) =>{
-    //get user and add custom claim (admin); since it is a promise it might not return immediately
-    console.log("hey the add admin role has been called")
-    return admin.auth().getUserByEmail(data.email).then(user => {
-        admin.auth().setCustomUserClaims(user.uid, {
-            admin: true, //assigning the admin: true value pair to the custom user @ uid
-        });
-    }).then( () => {
-        return { // when the person has been made an admin, return a message
-            message: `Success!  ${data.email} has been made an admin`
-        }
-    }).catch(err => {
-        console.log("there was an error")
-        console.log(err)
-        return err;
-    });
-});
+//Take out the admin stuff so it works on safari
+// if (typeof window !== "undefined"){
+//     const functions = require("firebase-functions");
+//     const admin = require("firebase-admin")
+//
+//     admin.initializeApp()
+//
+// // data is the user information like email or username
+// // context authentication information from firebase
+// //exports is used to send out a function, https on call just means the method is onCall
+// // or availible from the frontend
+// //Net Ninja tutorial: https://www.youtube.com/watch?v=4wa3CMK4E2Y
+//     exports.addAdminRole = functions.https.onCall((data, context) =>{
+//         //get user and add custom claim (admin); since it is a promise it might not return immediately
+//         console.log("hey the add admin role has been called")
+//         return admin.auth().getUserByEmail(data.email).then(user => {
+//             admin.auth().setCustomUserClaims(user.uid, {
+//                 admin: true, //assigning the admin: true value pair to the custom user @ uid
+//             });
+//         }).then( () => {
+//             return { // when the person has been made an admin, return a message
+//                 message: `Success!  ${data.email} has been made an admin`
+//             }
+//         }).catch(err => {
+//             console.log("there was an error")
+//             console.log(err)
+//             return err;
+//         });
+//     });
+// }
--- dash-vue2-alpha/src/components/teaching_resources/TeachingMain.vue
+++ dash-vue2-alpha/src/components/teaching_resources/TeachingMain.vue
@@ -6,19 +6,33 @@
       <v-expansion-panel>
         <v-expansion-panel-header>
           <v-col cols="11">
-            <h2>Teaching Resources</h2>
+            <h2>Curriculum Dashboard: Serendipity Backpack</h2>
           </v-col>
         </v-expansion-panel-header>
 
         <v-expansion-panel-content>
-          <v-list-item :href=" `https://bit.ly/serendipitySheet`" target="_blank">Schedule and Sign Ups</v-list-item>
-          <v-list-item :href=" `https://docs.google.com/document/d/178hD_GJt7F6s6zoXuBdKOhOvl7tQYQrfk_CzOCP3VHo/edit?usp=sharing`" target="_blank">Curriculum Template
+          <v-list-item :href=" `https://bit.ly/serendipitySheet`" target="_blank">Create your own curriculum repository to streamline teaching resources, especially during Distance Learning</v-list-item>
+          <v-list-item :href=" `https://docs.google.com/document/d/178hD_GJt7F6s6zoXuBdKOhOvl7tQYQrfk_CzOCP3VHo/edit?usp=sharing`" target="_blank">Inspired by Serendipity STEM's Special Education outreach in STEM curriculum development
           </v-list-item>
-          <v-list-item :href="`https://docs.google.com/document/d/15yU0rux8ty0kOUO1TVZrNz9ihbC47s-Bzt3gSuBWiAg/edit?usp=sharing`" target="_blank">Teaching Process
-          </v-list-item>
-          <v-list-item :href="`https://scratch.mit.edu`" target="_blank">Scratch (Username: SerendipityOutreach | Password: Ask a Veteran Teacher)
+          <v-list-item :href="`https://docs.google.com/document/d/15yU0rux8ty0kOUO1TVZrNz9ihbC47s-Bzt3gSuBWiAg/edit?usp=sharing`" target="_blank">Project for Ms. Berbawy's Principle of Engineering Course
           </v-list-item>
         </v-expansion-panel-content>
+<!--      <v-expansion-panel>-->
+<!--        <v-expansion-panel-header>-->
+<!--          <v-col cols="11">-->
+<!--            <h2>Teaching Resources</h2>-->
+<!--          </v-col>-->
+<!--        </v-expansion-panel-header>-->
+
+<!--        <v-expansion-panel-content>-->
+<!--          <v-list-item :href=" `https://bit.ly/serendipitySheet`" target="_blank">Schedule and Sign Ups</v-list-item>-->
+<!--          <v-list-item :href=" `https://docs.google.com/document/d/178hD_GJt7F6s6zoXuBdKOhOvl7tQYQrfk_CzOCP3VHo/edit?usp=sharing`" target="_blank">Curriculum Template-->
+<!--          </v-list-item>-->
+<!--          <v-list-item :href="`https://docs.google.com/document/d/15yU0rux8ty0kOUO1TVZrNz9ihbC47s-Bzt3gSuBWiAg/edit?usp=sharing`" target="_blank">Teaching Process-->
+<!--          </v-list-item>-->
+<!--          <v-list-item :href="`https://scratch.mit.edu`" target="_blank">Scratch (Username: SerendipityOutreach | Password: Ask a Veteran Teacher)-->
+<!--          </v-list-item>-->
+<!--        </v-expansion-panel-content>-->
 
       </v-expansion-panel>
     </v-expansion-panels>
